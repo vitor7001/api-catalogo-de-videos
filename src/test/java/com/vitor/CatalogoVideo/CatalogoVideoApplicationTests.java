53c0b04729a11fae8b9ddb7bff2cc52ef80a2d31
vitorgomes7001 <vitorgomes7001@gmail.com>
2022-02-22 19:15:59 -0300

    feat: setup inicial e primeirar dependencias

---
+++ src/test/java/com/vitor/CatalogoVideo/CatalogoVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.CatalogoVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
